7a3570cabb280049b023cc25b4590249be99f49d
renraelreve <123564916+renraelreve@users.noreply.github.com>
2024-07-14 10:26:44 +0800

    update assignment with binding

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_geek():
-    return '<h1>Hello from Flask & Docker</h1>'
-
+    return '<h1>Hello from Flask & Docker, haha!</h1>'
 
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", debug=True)
